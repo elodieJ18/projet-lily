75db8f09e974d00382c7c76cbdd8381e92251ab1
Elodie Jean <elodie.jean18@gmail.com>
2022-05-12 22:32:18 +0200

    animation

--- animation.js
+++ animation.js
@@ -1,11 +1,27 @@
 const allWorks = document.querySelector(".works-content-all");
 const openWorks = document.querySelector(".second-img");
 const cashCover = document.querySelector(".cash-presentation");
+const hoverClick = document.querySelector(".hoverclick");
+const firstImg = document.querySelector(".first-img");
+const thirdImg = document.querySelector(".third-img");
+
+let active = false;
 
 openWorks.addEventListener("click", () => {
-  if ((allWorks.style.display = "none")) {
-    allWorks.style.display = "flex";
-    cashCover.remove();
-  }
-  document.querySelector(".works-content-all").style.display = "flex";
+  active = !active;
+  active
+    ? allWorks.classList.add("active")
+    : allWorks.classList.remove("active");
+  active
+    ? cashCover.classList.add("active")
+    : cashCover.classList.remove("active");
+  active
+    ? hoverClick.classList.add("active")
+    : hoverClick.classList.remove("active");
+  active
+    ? firstImg.classList.add("active")
+    : firstImg.classList.remove("active");
+  active
+    ? thirdImg.classList.add("active")
+    : thirdImg.classList.remove("active");
 });
