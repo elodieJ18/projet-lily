83d7a486fb8983c3a5bbc9061949f97fad33bc53
Elodie Jean <elodie.jean18@gmail.com>
2022-05-14 17:32:36 +0200

    mouse-animation

--- animation.js
+++ animation.js
@@ -15,9 +15,9 @@ openWorks.addEventListener("click", () => {
   active
     ? allWorks.classList.add("active")
     : allWorks.classList.remove("active");
-  active
-    ? cashCover.classList.add("active")
-    : cashCover.classList.remove("active");
+  //active
+  //? cashCover.classList.add("active")
+  //: cashCover.classList.remove("active");
   active
     ? hoverClick.classList.add("active")
     : hoverClick.classList.remove("active");
@@ -32,3 +32,34 @@ openWorks.addEventListener("click", () => {
     : imgClose.classList.remove("active");
   active ? imgOpen.classList.add("active") : imgOpen.classList.remove("active");
 });
+
+/*----------animation curseur--------- */
+
+const circleSvg = document.querySelector("svg");
+const btn = document.querySelector("button");
+
+let mouseX = 0;
+let mouseY = 0;
+//let intv = 0
+
+window.addEventListener("mousemove", (event) => {
+  // circleSvg.style.top = event.clientY - 45;
+  // circleSvg.style.left = event.clientX - 45;
+
+  mouseY = event.clientY / 16 - 45 / 16 + "rem";
+  mouseX = event.clientX / 16 - 45 / 16 + "rem";
+});
+
+const mouseMove = () => {
+  //intv += 1
+
+  circleSvg.style.top = mouseY;
+  circleSvg.style.left = mouseX;
+
+  //circleSvg.style.opacity = 1 + Math.sin(intv * .04)
+  //console.log(Math.sin(intv * .04))
+
+  window.requestAnimationFrame(mouseMove);
+};
+
+mouseMove();
